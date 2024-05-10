eb7bd80f3612bc638aee9a2cd298c22a38b50c42
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2024-05-10 10:06:35 +0200

    added api calls

--- src/lib/useApi.js
+++ src/lib/useApi.js
@@ -1,13 +1,33 @@
 import axios from 'axios'
 
+const sleep = (ms) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  })
+}
+
 export const getLoginUrl = async (userType) => {
-  console.log(`${import.meta.env.VITE_ONBOARDING_API_URI}/LoginUrl?userType=${userType}`)
   const { data } = await axios.get(`${import.meta.env.VITE_ONBOARDING_API_URI}/LoginUrl?userType=${userType}`, { headers: { 'x-functions-key': import.meta.env.VITE_ONBOARDING_API_PUBLIC_KEY } })
   return data
 }
 
+export const getLogoutUrl = async () => {
+  if (import.meta.env.VITE_MOCK_API && import.meta.env.VITE_MOCK_API === 'true') {
+    return { logoutUrl: '/' }
+  }
+  const { data } = await axios.get(`${import.meta.env.VITE_ONBOARDING_API_URI}/LogoutUrl`, { headers: { 'x-functions-key': import.meta.env.VITE_ONBOARDING_API_PUBLIC_KEY } })
+  return data
+}
+
 export const resetPassword = async (code, iss, state) => {
-  console.log(`${import.meta.env.VITE_ONBOARDING_API_URI}/ResetPassword`)
+  if (import.meta.env.VITE_MOCK_API && import.meta.env.VITE_MOCK_API === 'true') {
+    await sleep(6000)
+    return {
+      displayName: 'Mock Trynefjert',
+      userPrincipalName: 'mock.trynefjerten.sorensen@fisfylke.no',
+      maskedPhoneNumber: '+47 *****123'
+    }
+  }
   const { data } = await axios.post(`${import.meta.env.VITE_ONBOARDING_API_URI}/ResetPassword`, { code, iss, state }, { headers: { 'x-functions-key': import.meta.env.VITE_ONBOARDING_API_PUBLIC_KEY } })
   return data
 }
