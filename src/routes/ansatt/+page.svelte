3965b8db4acc81b9d2e6f719637e2d7f3d650f40
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2024-05-13 13:58:23 +0200

    errors

--- src/routes/ansatt/+page.svelte
+++ src/routes/ansatt/+page.svelte
@@ -4,15 +4,22 @@
   import { goto } from '$app/navigation'
   import key from '$lib/assets/key.svg'
 
+  let errorMessage = null
+
   const redirect = async () => {
     // const confirmation = confirm("Er du sikker på du vil resette ditt passord?")
     const confirmation = true
+    errorMessage = null
     if (confirmation) {
       if (import.meta.env.VITE_MOCK_API && import.meta.env.VITE_MOCK_API === 'true') {
         goto('/mockidporten', { replaceState: false, invalidateAll: true })
       } else {
-        const { loginUrl } = await getLoginUrl('ansatt')
-        window.location.href = loginUrl
+        try {
+          const { loginUrl } = await getLoginUrl('ansatt')
+          window.location.href = loginUrl
+        } catch (error) {
+          errorMessage = error.response?.data?.message || error.toString()
+        }
       }
     }
   }
@@ -25,6 +32,12 @@
     <p><strong>MERK:</strong> SMS med engangspassord vil bli sendt til telefonnummeret du har registrert i kontakt og reservasjons-registeret. Sjekk hva du har registrert i kontakt og reservasjons-registeret her: <a href="https://minprofil.kontaktregisteret.no" target="_blank">minprofil.kontaktregisteret.no</a></p>
   </div>
   -->
+  {#if errorMessage}
+    <div class="error">
+      <h3 class="errorTitle">Oi, noe gikk galt 😩</h3>
+      <p>{errorMessage}</p>
+    </div>
+  {/if}
   <div class="centerstuff">
     <CardButton header={'Tilbakestill mitt passord, og send engangspassord på sms'} imgPath={key} imgAlt={'Ikon bilde av en nøkkel'} gotoPath={''} paragraph={'Krever pålogging med MinID eller BankID'} boolValue={false} func={redirect}/>
   </div>
@@ -37,5 +50,10 @@
       flex-direction: row;
       flex-wrap: wrap;
       justify-content: center;
+  }
+  .error {
+    text-align: center;
+    background-color: var(--nype-10);
+    padding: 16px;
   }
 </style>
