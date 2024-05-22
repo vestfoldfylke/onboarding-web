2ee47bddd5e90b39d46c45cf33733d0a8b320df4
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2024-05-22 10:02:59 +0200

    added Infobox

---
+++ src/lib/components/InfoBox.svelte
@@ -0,0 +1,47 @@
+<script>
+    // Props
+    export let title
+    export let content
+
+    export let open = false
+
+</script>
+
+<div class="infoBox">
+    <button class="title" on:click={() => { open = !open } }><span class="material-symbols-outlined">{open ? 'keyboard_arrow_down' : 'chevron_right'}</span><strong>{title}</strong></button>
+    {#if open}
+        <div class="content">
+            <slot></slot>
+        </div>
+    {/if}
+</div>
+
+<style>
+    .infoBox {
+        background-color: var(--himmel-10);
+    }
+    .title {
+        border: 0px;
+        border-radius: 0px;
+        padding: 5px;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        background-color: var(--himmel-30);
+    }
+    .title:hover {
+        cursor: pointer;
+        background-color: var(--himmel-50);
+    }
+    .content {
+        padding: 12px;
+    }
+
+
+    @media only screen and (max-width: 768px) {
+        /* For mobile phones: */
+        .title {
+            width: 100%;
+        }
+    }  
+</style>
