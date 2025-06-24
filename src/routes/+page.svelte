8cab1c4d01d2a5317c9fdc5a88da78f77d1f4713
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2025-06-24 11:59:51 +0200

    reorder foreign access request section for improved clarity

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,13 +8,6 @@
 </script>
 
 <main>
-    <div class="centerstuff">
-        <h3>Logg på med MinID eller BankID for å aktivere brukeren din eller tilbakestille passordet ditt. </h3>
-    </div>
-    <div class="centerstuff">
-        <CardButton header={'Elev eller lærling'} imgPath={school} imgAlt={'Ikon bilde av en elev'} gotoPath={'/elev'} paragraph={'Trykk her hvis du er elev eller lærling'} boolValue={true}/>
-        <CardButton header={'Ansatt'} imgPath={person} imgAlt={'Ikon bilde av en ansatt'} gotoPath={'/ansatt'} paragraph={'Trykk her hvis du er ansatt'} boolValue={true}/>
-    </div>
     {#if utenlandsreiseUrl}
         <div class="centerstuff">
             <h3>Logg på med MinID eller BankID for å bestille tilgang for pålogging fra utlandet.</h3>
@@ -23,6 +16,13 @@
             <CardButton header={'Ansatt eller elev'} imgPath={travel} imgAlt={'Ikon bilde av et fly'} paragraph={'Trykk her hvis du ønsker å logge deg på fylkeskommunens systemer fra utlandet'} boolValue={false} func={() => {window.open(utenlandsreiseUrl)}}/>
         </div>
     {/if}
+    <div class="centerstuff">
+        <h3>Logg på med MinID eller BankID for å aktivere brukeren din eller tilbakestille passordet ditt. </h3>
+    </div>
+    <div class="centerstuff">
+        <CardButton header={'Elev eller lærling'} imgPath={school} imgAlt={'Ikon bilde av en elev'} gotoPath={'/elev'} paragraph={'Trykk her hvis du er elev eller lærling'} boolValue={true}/>
+        <CardButton header={'Ansatt'} imgPath={person} imgAlt={'Ikon bilde av en ansatt'} gotoPath={'/ansatt'} paragraph={'Trykk her hvis du er ansatt'} boolValue={true}/>
+    </div>
 </main>
 
 
